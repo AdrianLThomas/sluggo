e2c86318b3f1c47f4ea20363c3b818065c629beb
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2026-08-01 11:28:43 +0000

    structure

---
+++ game/game.go
@@ -0,0 +1,25 @@
+package game
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type game struct {
+}
+
+func (g game) Update() error {
+	return nil
+}
+
+func (g game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func NewGame() *game {
+	return &game{}
+}
