1b7db46e379d63a4752b135c846521f39a771640
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2026-08-16 14:32:27 +0100

    feat: reduce object scale

--- game/objects/object.go
+++ game/objects/object.go
@@ -26,10 +26,13 @@ func (o *GameObject) Update() error {
 func (o *GameObject) Draw(screen *ebiten.Image, tileSize int, offsetX int, offsetY int) {
 	bounds := o.sprite.Bounds()
 
+	reduceBy := 1.7
 	op := &ebiten.DrawImageOptions{}
-	scale := float64(tileSize) / float64(bounds.Dx())
+	scale := float64(tileSize) / float64(bounds.Dx()) / reduceBy
+	scaled := float64(bounds.Dx()) * scale
+	centered := (float64(tileSize) - scaled) / 2
 	op.GeoM.Scale(scale, scale)
-	op.GeoM.Translate(float64((tileSize*o.position.X)+offsetX), float64(tileSize*o.position.Y+offsetY))
+	op.GeoM.Translate(float64(tileSize*o.position.X)+float64(offsetX)+centered, float64(tileSize*o.position.Y+offsetY)+centered)
 
 	screen.DrawImage(o.sprite, op)
 }
